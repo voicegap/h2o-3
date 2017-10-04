fcd28f3f89132f30315d924abcd351ed04674a15
wendycwong <wendycwong@yahoo.com>
2017-10-04 16:32:04 -0700

    PUBDEV-4697: early stop.  Added pyunit test from user example. PUBDEV-4697: early stopping bug.  Fixed bug in early_stopping when metric hits 0 for less is better, it should return true as there is nothing more we can do to improve it. PUBDEV-4697: early stop.  take care of the case for deviance which is decreasing but can be negative. PUBDEV-4697: early stop.  Change tolerance back to what the user was using. PUBDEV-4697: early stop.  Incorporate Arno code review suggestion.

--- h2o-core/src/main/java/hex/ScoreKeeper.java
+++ h2o-core/src/main/java/hex/ScoreKeeper.java
@@ -10,6 +10,8 @@ import water.util.MathUtils;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static hex.ScoreKeeper.StoppingMetric.deviance;
+
 /**
  * Low-weight keeper of scores
  * solely intended for display (either direct or as helper to create scoring history TwoDimTable).
@@ -105,7 +107,7 @@ public class ScoreKeeper extends Iced {
     if (len < 2*k) return false; //need at least k for SMA and another k to tell whether the model got better or not
 
     if (criterion==StoppingMetric.AUTO) {
-      criterion = classification ? StoppingMetric.logloss : StoppingMetric.deviance;
+      criterion = classification ? StoppingMetric.logloss : deviance;
     }
 
     boolean moreIsBetter = moreIsBetter(criterion);
@@ -191,7 +193,8 @@ public class ScoreKeeper extends Iced {
       Log.info("Windowed averages (window size " + k + ") of " + what + " " + (k+1) + " " + criterion.toString() + " metrics: " + Arrays.toString(movingAvg));
 
     double ratio = bestInLastK / lastBeforeK;
-    if (Double.isNaN(ratio)) return false;
+    if (Double.isNaN(ratio))
+      return !(moreIsBetter || criterion.equals(deviance));  // lastBeforeK=0 for less is better, no more improvement possible, return true
     boolean improved = moreIsBetter ? ratio > 1+rel_improvement : ratio < 1-rel_improvement;
 
     if (verbose)
